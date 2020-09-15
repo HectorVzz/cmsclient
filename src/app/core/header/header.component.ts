6d7e0a211e0ad014b98abb1b3ccb2630ac39ecda
Hector David Vazquez Soto <hdvs96@hotmail.com>
2020-09-15 12:01:27 -0500

    header title comes from the server now , also body data from the main component , and fixed some styles

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { _ParseAST } from '@angular/compiler';
 
+interface INavData {
+  title: string;
+}
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +15,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
+
+  navData: INavData;
 
   ngOnInit(): void {
+    this.getNavData().subscribe((data) => {
+      this.navData = data['data'];
+    })
+  }
+
+
+  getNavData() {
+    return this.http.get('http://localhost:5000/navdata', { responseType: 'json', observe: 'body' });
   }
 
 }
--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -5,10 +5,15 @@ import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { _ParseAST } from '@angular/compiler';
 
-interface Iresponse{
+interface IPost {
   title: string;
   description: string;
   date: Date;
+}
+
+interface IBodyData {
+  title: string;
+  description: string;
 }
 
 @Component({
@@ -23,17 +28,26 @@ export class MainComponent implements OnInit {
 
   }
 
-  posts: any;
-  aver: any;
+  posts: IPost[];
+  bodyData: IBodyData;
 
   async ngOnInit(): Promise<void> {
     this.getPosts().subscribe((data) => {
       this.posts = data['data'];
     });
+    this.getBodyData().subscribe((data) => {
+      this.bodyData = data['data'];
+    })
   }
 
-   getPosts(){
-    return this.http.get<[Iresponse]>('http://localhost:5000/post',{responseType: 'json', observe: 'body'})
+
+
+  getPosts() {
+    return this.http.get('http://localhost:5000/post', { responseType: 'json', observe: 'body' });
+  }
+
+  getBodyData() {
+    return this.http.get('http://localhost:5000/bodydata', { responseType: 'json', observe: 'body' });
   }
 
 }
